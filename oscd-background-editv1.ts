163d76de0e07bc6453bffa16d7218943e49ba8e8
stee-re <stephen.renwick@omicronenergy.com>
2025-07-03 14:04:53 +0200

    feat: initial commit
    Release-As: 0.0.1

--- oscd-background-editv1.ts
+++ oscd-background-editv1.ts
@@ -2,25 +2,14 @@ import { EditEvent } from '@omicronenergy/oscd-api';
 import { convertEdit } from '@omicronenergy/oscd-api/utils.js';
 import { XMLEditor } from '@omicronenergy/oscd-editor';
 
-class OscdBackgroundEditV1 extends HTMLElement {
-  private xmlEditor!: XMLEditor;
+export default class OscdBackgroundEditV1 extends HTMLElement {
+  editor!: XMLEditor;
 
   constructor() {
     super();
     document.addEventListener('oscd-edit', (event: Event) => {
       const editV2 = convertEdit((event as EditEvent).detail);
-
       this.editor.commit(editV2);
     });
   }
-
-  set editor(value: XMLEditor) {
-    this.xmlEditor = value;
-  }
-
-  get editor(): XMLEditor {
-    return this.xmlEditor;
-  }
 }
-
-export default OscdBackgroundEditV1;
